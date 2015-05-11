d354d143e2bb95a6cf7938f9c956b939e0f700fa
michael153 <michaelruihaowan@gmail.com>
2015-05-10 17:12:59 -0700

    Execute this file

---
+++ SickNick.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.applet.Applet;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+
+public class SickNick extends JFrame
+{
+	public static void main(String[] args)
+	{
+		SickNick a = new SickNick();
+	}
+
+	public SickNick()
+	{
+		super("Sick Nick");
+		setSize(1280, 720);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		HomePage b = new HomePage(); //instantiate
+		b.setBorder(BorderFactory.createEmptyBorder(-15, 0, 0, 0)); //insets, set top inset to -15 to compensate for default blank space
+		setContentPane(b);
+		//because some panels use null layout and have "magic numbers",
+		//set resizable to false
+		setResizable(false);
+		setVisible(true);
+	}
+}
